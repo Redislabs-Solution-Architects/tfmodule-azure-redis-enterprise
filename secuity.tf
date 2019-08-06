311a5fcf3f6ad678bca9ba4375fdda68d6e2401a
Chris Mague <github@mague.com>
2019-08-06 13:36:00 -0700

    update sgs to allow for more dbs and prometheus

--- secuity.tf
+++ secuity.tf
@@ -28,6 +28,19 @@ resource "azurerm_network_security_group" "sg" {
     destination_address_prefix = "*"
   }
 
+
+  security_rule {
+    name                       = "DB-10003"
+    priority                   = 1009
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "10003"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   security_rule {
     name                       = "DB-10002"
     priority                   = 1002
@@ -63,6 +76,19 @@ resource "azurerm_network_security_group" "sg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "Prometheus"
+    priority                   = 1007
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8070"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 
   tags = merge({ Name = "${var.net-name}" }, var.common-tags)
 
