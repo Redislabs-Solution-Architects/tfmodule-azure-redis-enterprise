aca2b7697fa91f7b70be05c5e42f46b7b3b31fa5
Chris Mague <github@mague.com>
2019-08-15 11:51:50 -0700

    add new security rules

--- secuity.tf
+++ secuity.tf
@@ -89,6 +89,33 @@ resource "azurerm_network_security_group" "sg" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "DB-10000"
+    priority                   = 1017
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "10000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "DNS-Server"
+    priority                   = 1027
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_range     = "53"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+
+
+
 
   tags = merge({ Name = "${var.net-name}" }, var.common-tags)
 
