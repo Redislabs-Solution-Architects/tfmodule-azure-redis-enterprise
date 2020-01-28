a31bff53ae8342aef8a7f08745e690bf1f657e1b
Michael Kaiser <mkaiser@incomm.com>
2020-01-28 17:20:36 -0500

    Update branched template

--- dns.tf
+++ dns.tf
@@ -1,31 +1,9 @@
-locals {
-  full_zone = (var.cluster-base-resource-group == null ? 1 : 0)
-}
-
-# If there's no upstream record to update, create one
-resource "azurerm_dns_zone" "base" {
-  count               = local.full_zone
-  name                = "${var.cluster-base-domain}"
-  resource_group_name = "${azurerm_resource_group.resource.name}"
-}
-
-resource "azurerm_dns_a_record" "fixedip" {
-  count = var.node-count
-  name = "ns${count.index}-${var.cluster-name}"
-  zone_name           = "${var.cluster-base-domain}"
-  resource_group_name  = "${(var.cluster-base-resource-group != null ? var.cluster-base-resource-group : azurerm_resource_group.resource.name)}"
-  records =  [ "${element(data.azurerm_public_ip.fixedip.*.ip_address, count.index)}" ]
-  # records = [ "${element(azurerm_network_interface.nic.*.private_ip_address, count.index)}" ]
-  ttl                 = 300
-}
-
-# TODO - allow switching between public and private IP addressing in DNS
 resource "azurerm_dns_ns_record" "fixedip" {
-  count = var.node-count
+  count               = var.node-count
   name                = "${var.cluster-name}"
   zone_name           = "${var.cluster-base-domain}"
-  resource_group_name  = "${(var.cluster-base-resource-group != null ? var.cluster-base-resource-group : azurerm_resource_group.resource.name)}"
+  resource_group_name = "${(var.cluster-base-resource-group != null ? var.cluster-base-resource-group : azurerm_resource_group.resource.name)}"
   ttl                 = 300
   # Has to have trailing periods on each record
-  records = formatlist("%s.${var.cluster-base-domain}.", azurerm_dns_a_record.fixedip.*.name)
+  records             = formatlist("%s.${var.cluster-base-domain}.", azurerm_dns_a_record.fixedip.*.name)
 }
--- mysettings.tfvars
+++ mysettings.tfvars
@@ -1,16 +1,17 @@
-location    = "westus2"
-av_zone     = ["1","2"]
-net-name    = "redis-test"
-net-cidr    = ["10.0.2.0/24"]
-node-count  = 3
-node-size   = "Standard_DS3_v2"
-common-tags = { Config = "terraform", Environment = "tf-test" }
-cluster-base-domain = "kaiser.guru"
-cluster-name = "redis-test-mgk"
-password = "EVENmoarSECURE"
-node-publisher = "RedHat"
-node-offer     = "RHEL"
-node-sku       = "7-RAW"
-node-version   = "latest"
-re-download-url = "https://s3.amazonaws.com/redis-enterprise-software-downloads/5.4.6/redislabs-5.4.6-11-rhel7-x86_64.tar"
-demodb-name = "first-db"
+location                        = "westus2"
+av_zone                         = ["1","2"]
+net-name                        = "redis-test"
+net-cidr                        = ["10.0.2.0/24"]
+node-count                      = 3
+node-size                       = "Standard_DS3_v2"
+common-tags                     = { Config = "terraform", Environment = "tf-test" }
+cluster-base-domain             = "kaiser.guru"
+cluster-base-resource-group     = "azuse-kaiserguru-dns-rg-prod"
+cluster-name                    = "redis-test-mgk"
+password                        = "EVENmoarSECURE"
+node-publisher                  = "RedHat"
+node-offer                      = "RHEL"
+node-sku                        = "7-RAW"
+node-version                    = "latest"
+re-download-url                 = "https://s3.amazonaws.com/redis-enterprise-software-downloads/5.4.6/redislabs-5.4.6-11-rhel7-x86_64.tar"
+demodb-name                     = "first-db"
