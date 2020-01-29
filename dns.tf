4b1658fca07d051880d90174f22d44b4416139da
Michael Kaiser <mkaiser@incomm.com>
2020-01-28 20:26:35 -0500

    Fix up error with dns

--- dns.tf
+++ dns.tf
@@ -1,3 +1,13 @@
+resource "azurerm_dns_a_record" "fixedip" {
+  count = var.node-count
+  name = "ns${count.index}-${var.cluster-name}"
+  zone_name           = "${var.cluster-base-domain}"
+  resource_group_name  = "${(var.cluster-base-resource-group != null ? var.cluster-base-resource-group : azurerm_resource_group.resource.name)}"
+  records =  [ "${element(data.azurerm_public_ip.fixedip.*.ip_address, count.index)}" ]
+  # records = [ "${element(azurerm_network_interface.nic.*.private_ip_address, count.index)}" ]
+  ttl                 = 300
+}
+
 resource "azurerm_dns_ns_record" "fixedip" {
   count               = var.node-count
   name                = "${var.cluster-name}"
