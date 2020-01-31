0883203bb177d4b07afa03c4cd82d128eb1a1088
Michael Kaiser <mkaiser@incomm.com>
2020-01-30 23:40:00 -0500

    Removed count from  azurerm_dns_ns_record.fixedip because we are only adding one of this record.  Makes "destroy" run clean.

--- dns.tf
+++ dns.tf
@@ -13,12 +13,10 @@ resource "azurerm_dns_a_record" "fixedip" {
   }
 }
 
-resource "azurerm_dns_ns_record" "fixedip" {
-  count               = var.node-count
+resource "azurerm_dns_ns_record" "fixedip" {  
   name                = "${var.cluster-name}"
   zone_name           = "${var.cluster-base-domain}"
   resource_group_name = "${(var.cluster-base-resource-group != null ? var.cluster-base-resource-group : azurerm_resource_group.resource.name)}"
   ttl                 = 300
-  # Has to have trailing periods on each record
   records             = formatlist("%s.${var.cluster-base-domain}.", azurerm_dns_a_record.fixedip.*.name)    
 }
