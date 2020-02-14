5002e2c058cb93d198d12b7e832c0c84c2feeac2
Michael Kaiser <mkaiser@incomm.com>
2020-02-13 19:58:47 -0500

    scaled up the test boxes

--- modules/redis/linux.tf
+++ modules/redis/linux.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_machine" "redis-client" {
   location              = var.location
   resource_group_name   = azurerm_resource_group.resource.name
   network_interface_ids = [ azurerm_network_interface.nic-client.id ]
-  vm_size               = "Standard_B2s"
+  vm_size               = "Standard_D2s_v3"
 
   storage_os_disk {
     name              = "${var.net-name}-client"
