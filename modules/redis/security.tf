84411caff40aa0c3d062ac6c55a34c1100f2a0e0
Michael Kaiser <mkaiser@incomm.com>
2020-02-25 13:45:39 -0500

    Allow ping

--- modules/redis/security.tf
+++ modules/redis/security.tf
@@ -7,6 +7,18 @@ resource "azurerm_network_security_group" "sg" {
   name                = var.net-name
   location            = var.location
   resource_group_name = azurerm_resource_group.resource.name
+
+  security_rule {    
+    name                       = "Ping"
+    priority                   = 700
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"    
+    destination_address_prefix = "*"
+  }
   
   security_rule {    
     name                       = "SSH-Serve"
