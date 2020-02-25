a5e5b9f8beffaab0d49fb9155cf19ffcea947e5e
Michael Kaiser <mkaiser@incomm.com>
2020-02-24 22:23:50 -0500

    fixup error

--- modules/redis/network.tf
+++ modules/redis/network.tf
@@ -12,7 +12,7 @@ resource "azurerm_virtual_network" "network" {
 }
 
 output "network_id" {
-  value = azurerm_virtual_network.network.idz`
+  value = azurerm_virtual_network.network.id
 }
 
 resource "azurerm_subnet" "subnet" {
