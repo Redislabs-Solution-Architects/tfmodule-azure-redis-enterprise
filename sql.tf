46dfff0edf5d2591e759826662cc840232c3c4d2
Michael Kaiser <mkaiser@incomm.com>
2020-03-02 15:07:15 -0500

    Create SQL Server

--- sql.tf
+++ sql.tf
@@ -8,21 +8,31 @@ resource "azurerm_sql_server" "mgk-sql-server" {
 }
 
 resource "azurerm_sql_database" "mgk-sql" {
-  name                  = "mgk-sql"
-  resource_group_name   = azurerm_resource_group.azuse2-devops-mgk-rg.name
-  location              = azurerm_resource_group.azuse2-devops-mgk-rg.location
-  server_name           = azurerm_sql_elasticpool.mgk-sql-pool.name
-  tags                  = local.tags  
+  name                              = "mgk-sql"
+  resource_group_name               = azurerm_resource_group.azuse2-devops-mgk-rg.name
+  location                          = azurerm_resource_group.azuse2-devops-mgk-rg.location
+  server_name                       = azurerm_sql_server.mgk-sql-server.name
+  elastic_pool_name                 = azurerm_mssql_elasticpool.mgk-sql-pool.name  
+  edition                           = "Basic"
+  requested_service_objective_name  = "ElasticPool"  
+  tags                              = local.tags  
 }
 
-resource "azurerm_sql_elasticpool" "mgk-sql-pool" {
+resource "azurerm_mssql_elasticpool" "mgk-sql-pool" {
   name                  = "mgk-sql-pool"
   resource_group_name   = azurerm_resource_group.azuse2-devops-mgk-rg.name
   location              = azurerm_resource_group.azuse2-devops-mgk-rg.location
   server_name           = azurerm_sql_server.mgk-sql-server.name
-  edition               = "Basic"
-  dtu                   = 50
-  db_dtu_min            = 0
-  db_dtu_max            = 5  
-  //pool_size             = 32
+  max_size_gb           = 4.8828125 
+
+  sku {
+    name     = "BasicPool"
+    tier     = "Basic"    
+    capacity = 50
+  }
+
+  per_database_settings {
+    min_capacity = 0
+    max_capacity = 5
+  }
 }
