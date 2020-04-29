82d25d04c98f39b449d9a49e686f80c91d5478b3
Michael Kaiser <mkaiser@incomm.com>
2020-04-29 13:40:11 -0400

    Update deploy script

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ This set of Terraform scripts should be turned into a module,
 but for now it's a great starting point for Redis Enterprise.
 */
 provider "azurerm" {
-  version = ">=1.38.0"
+  version = ">y=1.38.0"
   partner_id = "aeede883-0195-5a05-849b-ad4c2153b7cf"   
   subscription_id = "beecf88d-33e8-4622-b73a-a7fc84b209c7"  
   features {}
