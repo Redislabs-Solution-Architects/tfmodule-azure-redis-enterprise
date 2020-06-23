32a1cd98e02e0373e5f52a1f9fb9e4354b9fcb60
Chris Mague <github@mague.com>
2020-06-23 10:49:00 -0700

    better outupts

--- output.tf
+++ output.tf
@@ -6,9 +6,6 @@ output "ui-links" {
   value = formatlist("https://%s:8443/", azurerm_public_ip.fixedip.*.ip_address)
 }
 
-# TODO: Show this when there are some?
-# output "dns-servers" {
-#   value = azurerm_dns_zone.base.name_servers
-# }
-
-# TODO: Pull the full hostname and generated password for the sample DB here
+output "node-external-ips" {
+  value = [azurerm_public_ip.fixedip.*.ip_address]
+}
